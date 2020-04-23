b49f6023db2a4a5cb536f031fdd4a2e1303deeda
MARIA IGNACIA PLAZA DE LOS REYES <miplazadelosreyes@uc.cl>
2020-04-23 15:09:40 -0300

    fixing HTTP codes

--- burgerapp/views.py
+++ burgerapp/views.py
@@ -27,9 +27,7 @@ def hamburguesa_list(request, format=None):
 
 @api_view(['GET', 'POST'])
 def ingrediente_list(request):
-    """
-        List all code snippets, or create a new snippet.
-        """
+
     if request.method == 'GET':
         snippets = Ingrediente.objects.all()
         serializer = IngredienteSerializer(snippets, many=True)
@@ -39,19 +37,24 @@ def ingrediente_list(request):
         serializer = IngredienteSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response("Ingrediente creado", status=status.HTTP_201_CREATED)
+        return Response("Input invalido", status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['DELETE'])
+@api_view(['GET', 'DELETE'])
 def ingrediente_detail(request, pk):
     try:
         ingrediente = Ingrediente.objects.get(pk=pk)
     except Ingrediente.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+        return Response("Ingrediente inexistente",
+                        status=status.HTTP_404_NOT_FOUND)
 
-    if request.method == 'DELETE':
+    if request.method == 'GET':
+        serializer = IngredienteSerializer(ingrediente)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    elif request.method == 'DELETE':
         ingrediente.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response("Ingrediente eliminado", status=status.HTTP_200_OK)
 
 
