c56a46f4f7736c5576a9e8cb2744da5a5c7beabf
MARIA IGNACIA PLAZA DE LOS REYES <miplazadelosreyes@uc.cl>
2020-04-30 17:16:19 -0300

    fixing url

--- burgerapp/urls.py
+++ burgerapp/urls.py
@@ -3,6 +3,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from burgerapp import views
 
 urlpatterns = [
+    path('', "Esta es la pagina de Hamburguesas para ver su contenido prueba con /hamburguesa o /ingrediente"),
     path('hamburguesa', views.hamburguesa_list),
     path('hamburguesa/<slug:pk>', views.hamburguesa_detail),
     path('ingrediente', views.ingrediente_list),
